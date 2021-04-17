5cbce0ce0bb998078626ca49ecfd0561416c8a97
lovelove33j1 <lovelove33j1@naver.com>
2021-04-17 21:37:01 +0900

    styled: 회원가입 오토레이아웃 잡기

--- iOS/YumYum/ViewControllers/SignUpButton.swift
+++ iOS/YumYum/ViewControllers/SignUpButton.swift
@@ -9,12 +9,11 @@ import UIKit
 
 class SignUpButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    required init(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)!
+        self.layer.cornerRadius = 5.0;
+        self.backgroundColor = .yellow
+        self.tintColor = .black
     }
-    */
-
+    
 }
