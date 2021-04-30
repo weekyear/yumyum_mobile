5d060c786a4a9f102c63dc0c88fca934ad7a77b6
Ahyeon Gil <ahyeonlog@gmail.com>
2021-05-01 02:48:19 +0900

    refactor : Feed 모델 Encodable, Decodable 익스텐션 분리

--- iOS/YumYum/Models/FeedModel.swift
+++ iOS/YumYum/Models/FeedModel.swift
@@ -8,25 +8,54 @@
 import Foundation
 
 
-struct Feed: Codable {
+struct Feed {
+    var userId: Int?
+    var title: String?
     var content: String?
-    var place: Place?
     var score: Int?
     var thumbnailPath: URL? = nil
-    var title: String?
-    var userId: Int?
     var videoPath: URL? = nil
     
+    //Decodable
+    var id: Int?
+    var likeCount: Int?
+    var isLike: Bool?
+    
+    // Encodable
+    var place: Place?
+    
+    
     enum CodingKeys: String, CodingKey {
+        case userId
+        case title
         case content
         case score
         case thumbnailPath
-        case title
-        case userId
         case videoPath
+        case id
+        case likeCount
+        case isLike
         case place = "placeRequest"
     }
     
 }
 
 
+extension Feed: Decodable {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+    }
+}
+
+extension Feed: Encodable {
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(userId, forKey: .userId)
+        try container.encode(title, forKey: .title)
+        try container.encode(content, forKey: .content)
+        try container.encode(score, forKey: .score)
+        try container.encode(thumbnailPath, forKey: .thumbnailPath)
+        try container.encode(videoPath, forKey: .videoPath)
+        try container.encode(place, forKey: .place)
+    }
+}
