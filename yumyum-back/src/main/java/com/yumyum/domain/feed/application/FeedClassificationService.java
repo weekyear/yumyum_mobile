9ca520f745d540ffdbe21ddd1117400c8e03063e
노천명 <shcjsaud3613@naver.com>
2021-05-06 07:04:25 +0900

    feat : 피드 검색 추가

---
+++ yumyum-back/src/main/java/com/yumyum/domain/feed/application/FeedClassificationService.java
@@ -0,0 +1,13 @@
+package com.yumyum.domain.feed.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class FeedClassificationService {
+
+
+}
