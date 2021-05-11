3d3d623d18f730969fbf7f5c5f3601061a1916db
AshOil <ka0@kakao.com>
2021-05-11 16:22:17 +0900

    fix: simple things

--- AOS/app/src/main/java/com/omnyom/yumyum/ui/selectedfeed/SelectedAllActivity.kt
+++ AOS/app/src/main/java/com/omnyom/yumyum/ui/selectedfeed/SelectedAllActivity.kt
@@ -1,34 +1,13 @@
 package com.omnyom.yumyum.ui.selectedfeed
 
-import android.content.Context
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Handler
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.BaseAdapter
 import androidx.activity.viewModels
-import androidx.core.net.toUri
 import androidx.lifecycle.observe
 import androidx.viewpager2.widget.ViewPager2
 import com.omnyom.yumyum.R
 import com.omnyom.yumyum.databinding.ActivitySelectedAllBinding
-import com.omnyom.yumyum.databinding.ListItemFoodBinding
-import com.omnyom.yumyum.helper.PreferencesManager
-import com.omnyom.yumyum.helper.RetrofitManager
 import com.omnyom.yumyum.helper.recycler.FlipFeedAdapter
-import com.omnyom.yumyum.model.feed.FeedData
-import com.omnyom.yumyum.model.like.LikeRequest
-import com.omnyom.yumyum.model.like.LikeResponse
 import com.omnyom.yumyum.ui.base.BaseBindingActivity
-import com.omnyom.yumyum.ui.userfeed.UserFeedActivity
-import com.omnyom.yumyum.ui.userfeed.UserFeedViewModel
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 
 class SelectedAllActivity : BaseBindingActivity<ActivitySelectedAllBinding>(R.layout.activity_selected_all) {
 
@@ -68,7 +47,5 @@ class SelectedAllActivity : BaseBindingActivity<ActivitySelectedAllBinding>(R.la
     }
 
     override fun release() {
-    }
-
     }
 }
