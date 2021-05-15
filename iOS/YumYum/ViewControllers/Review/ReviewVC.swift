33e7613dc3a4e96e2184c087def9585c31360965
lovelove33j1 <lovelove33j1@naver.com>
2021-05-16 02:34:31 +0900

    feat : 리뷰쓰기 애니메이션 적용
    Former-commit-id: cb6f2a932536d88555569f956bc1495d84e1dcf1

--- iOS/YumYum/ViewControllers/Review/ReviewVC.swift
+++ iOS/YumYum/ViewControllers/Review/ReviewVC.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SwiftyJSON
+import Lottie
 
 enum Score: Int {
     case one = 1
@@ -30,26 +31,39 @@ class ReviewVC: UIViewController {
     var place: Place?
     var tempfeed: Feed?
     
+    
+    @IBOutlet var scoreOneView: AnimationView!
+    @IBOutlet var scoreTwoView: AnimationView!
+    @IBOutlet var scoreThreeView: AnimationView!
+    @IBOutlet var scoreFourView: AnimationView!
+    @IBOutlet var scoreFiveView: AnimationView!
+    
+    let animationview = AnimationView(name: "ic_vomited")
+    let animationview2 = AnimationView(name: "ic_confused")
+    let animationview3 = AnimationView(name: "ic_neutral")
+    let animationview4 = AnimationView(name: "ic_lol")
+    let animationview5 = AnimationView(name: "ic_inloveface")
+    
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var contentTextField: UITextField!
     @IBOutlet var locationTextField: UITextField!
     @IBOutlet weak var emojiLabel: UILabel!
     
-    @IBAction func TabEmoji1(_ sender: Any) {
-        setEmoji(value: .one)
-    }
-    @IBAction func TabEmoji2(_ sender: Any) {
-        setEmoji(value: .two)
-    }
-    @IBAction func TabEmoji3(_ sender: Any) {
-        setEmoji(value: .three)
-    }
-    @IBAction func TabEmoji4(_ sender: Any) {
-        setEmoji(value: .four)
-    }
-    @IBAction func TabEmoji5(_ sender: Any) {
-        setEmoji(value: .five)
-    }
+//    @IBAction func TabEmoji1(_ sender: Any) {
+//        setEmoji(value: .one)
+//    }
+//    @IBAction func TabEmoji2(_ sender: Any) {
+//        setEmoji(value: .two)
+//    }
+//    @IBAction func TabEmoji3(_ sender: Any) {
+//        setEmoji(value: .three)
+//    }
+//    @IBAction func TabEmoji4(_ sender: Any) {
+//        setEmoji(value: .four)
+//    }
+//    @IBAction func TabEmoji5(_ sender: Any) {
+//        setEmoji(value: .five)
+//    }
     
     @IBAction func didTabLocationButton(_ sender: Any) {
         let vc = PlaceSearchVC.instance()
@@ -59,6 +73,9 @@ class ReviewVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUpAnimation()
+        setAnimationGesture()
         setLayout()
         titleTextField.delegate = self
         contentTextField.delegate = self
@@ -68,6 +85,114 @@ class ReviewVC: UIViewController {
         
         if tempfeed != nil {
             loadTempData()
+        }
+    }
+    
+    func setUpAnimation() {
+        
+        animationview.frame = CGRect(x: 0, y: 0, width: 45, height: 45)
+        animationview.contentMode = .scaleAspectFit
+        scoreOneView.addSubview(animationview)
+    
+        animationview2.frame = CGRect(x: 0, y: 0, width: 45, height: 45)
+        animationview2.contentMode = .scaleAspectFit
+        scoreTwoView.addSubview(animationview2)
+        
+        animationview3.frame = CGRect(x: 0, y: 0, width: 45, height: 45)
+        animationview3.contentMode = .scaleAspectFit
+        scoreThreeView.addSubview(animationview3)
+        
+        animationview4.frame = CGRect(x: 0, y: 0, width: 45, height: 45)
+        animationview4.contentMode = .scaleAspectFit
+        scoreFourView.addSubview(animationview4)
+
+        animationview5.frame = CGRect(x: 0, y: 0, width: 45, height: 45)
+        animationview5.contentMode = .scaleAspectFit
+        scoreFiveView.addSubview(animationview5)
+
+    }
+    
+    func setAnimationGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        
+        let tapGesture2 = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        
+        let tapGesture3 = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        
+        let tapGesture4 = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        
+        let tapGesture5 = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        animationview.tag = 1
+        animationview2.tag = 2
+        animationview3.tag = 3
+        animationview4.tag = 4
+        animationview5.tag = 5
+        animationview.addGestureRecognizer(tapGesture)
+        animationview2.addGestureRecognizer(tapGesture2)
+        animationview3.addGestureRecognizer(tapGesture3)
+        animationview4.addGestureRecognizer(tapGesture4)
+        animationview5.addGestureRecognizer(tapGesture5)
+        
+    }
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        let fillKeypath = AnimationKeypath(keypath: "**.**.Color" )
+        let redValueProvider = ColorValueProvider(Color(r: 1, g: 0.2, b: 0.3, a: 1))
+        
+        
+        switch sender.view?.tag {
+        case 1:
+            animationview.frame = CGRect(x: 0, y: 0, width: 45, height: 45)
+            animationview.setValueProvider(redValueProvider, keypath: fillKeypath)
+            animationview.play()
+            animationview.loopMode = .loop
+            animationview2.stop()
+            animationview3.stop()
+            animationview4.stop()
+            animationview5.stop()
+            setEmoji(value: .one)
+        case 2:
+            animationview2.frame = CGRect(x: 0, y: 0, width: 45, height: 45)
+            animationview2.setValueProvider(redValueProvider, keypath: fillKeypath)
+            animationview.stop()
+            animationview3.stop()
+            animationview4.stop()
+            animationview5.stop()
+            animationview2.play()
+            animationview2.loopMode = .loop
+            setEmoji(value: .two)
+        case 3:
+            animationview3.frame = CGRect(x: 0, y: 0, width: 45, height: 45)
+            animationview3.setValueProvider(redValueProvider, keypath: fillKeypath)
+            animationview3.loopMode = .loop
+            animationview2.stop()
+            animationview.stop()
+            animationview4.stop()
+            animationview5.stop()
+            animationview3.play()
+            setEmoji(value: .three)
+        case 4:
+            animationview4.frame = CGRect(x: 0, y: 0, width: 45, height: 45)
+            animationview4.setValueProvider(redValueProvider, keypath: fillKeypath)
+            animationview4.loopMode = .loop
+            animationview4.play()
+            animationview2.stop()
+            animationview3.stop()
+            animationview.stop()
+            animationview5.stop()
+            setEmoji(value: .four)
+        case 5:
+            animationview5.frame = CGRect(x: 0, y: 0, width: 45, height: 45)
+            animationview5.setValueProvider(redValueProvider, keypath: fillKeypath)
+            animationview5.play()
+            animationview5.loopMode = .loop
+            animationview2.stop()
+            animationview3.stop()
+            animationview4.stop()
+            animationview.stop()
+            setEmoji(value: .five)
+        default:
+            break
         }
     }
     
@@ -126,9 +251,9 @@ class ReviewVC: UIViewController {
                 self.feed.videoPath = URL(string: result["data"]["videoPath"].stringValue)
                 let user = UserDefaults.getLoginedUserInfo()
                 self.feed.userId = user!["id"].intValue
-                
+                self.feed.content = self.contentTextField.text
+                self.feed.title = self.titleTextField.text
                 self.feed.score = self.score.rawValue
-                
                 self.feed.place = self.place
                 
                 WebApiManager.shared.createFeed(feed: self.feed) { (result) in
