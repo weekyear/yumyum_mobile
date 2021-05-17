becdeff5e3addbcb579b20986e500835d9175ed4
AshOil <ka0@kakao.com>
2021-05-18 01:08:51 +0900

    feat: Messaging
    Former-commit-id: 7864ccbaf100b54e780bd0b79d1848d66802894f

--- AOS/app/src/main/java/com/omnyom/yumyum/MainActivity.kt
+++ AOS/app/src/main/java/com/omnyom/yumyum/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.omnyom.yumyum
 
 import android.content.Intent
+import android.util.Log
+import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
 import com.omnyom.yumyum.databinding.ActivityMainBinding
 import com.omnyom.yumyum.helper.PreferencesManager
 import com.omnyom.yumyum.helper.PreferencesManager.Companion.userId
@@ -16,6 +21,19 @@ class MainActivity : BaseBindingActivity<ActivityMainBinding>(R.layout.activity_
 
     override fun setup() {
         userId = PreferencesManager.getLong(this, "userId")?: 0
+
+        FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener { task ->
+            Log.d("plzzzzz", "${task.getResult()!!.token}")
+        }
+
+        Firebase.messaging.subscribeToTopic("$userId")
+                .addOnCompleteListener { task ->
+                    var msg = "성공"
+                    if (!task.isSuccessful) {
+                        msg = "실패"
+                    }
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                }
     }
 
     override fun setupViews() {
